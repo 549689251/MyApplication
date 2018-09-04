72c2251ef5c888107a3c624a14dbc3f9dfb5904f
gongdongdong <ahgw33@163.com>
2018-09-04 09:24:47 +0800

    [mod]2018-9-4,删除不需要的引用

--- app/src/main/java/com/demo/layload/myapplication/BaseActivity.java
+++ app/src/main/java/com/demo/layload/myapplication/BaseActivity.java
@@ -1,7 +1,6 @@
 package com.demo.layload.myapplication;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -9,7 +8,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
 import android.widget.Toast;
 
 /**
